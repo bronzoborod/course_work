f110c6d5b00f8a9abcaaf8102297ba0fbb44da50
Viktor <zasekre4no0@gmail.com>
2018-05-09 16:16:20 +0300

    finish

--- app/script/main.js
+++ app/script/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $('.menu_trigger').click(function() {
-        $('nav ul').slideToggle(500);
+        $('#basic_menu').slideToggle(500);
+        $('.nav_kind').click(function () {
+            $('.nav_kind_view').css('display','block !important');
+        })
     });//end slide toggle
 
     $(window).resize(function() {
@@ -14,7 +17,7 @@ $(document).ready(function() {
         $(window).resize(function () {
             $('#myAffix').width($('#left').width());
         });
-    });// end static seatch
+    });// end static search
 
     $(function () {
         $('#modal_date_popup').click(function () {
@@ -27,7 +30,12 @@ $(document).ready(function() {
         if ($(event.target).closest('#modal_date_popup').length || $(event.target).closest('.popup_date').length) return;
         $('.popup_date').fadeOut(500)
         event.stopPropagation();
-    })
+    });//end model window date
+
+    $('.content_item').mouseover(function () {
+        $('.item_info_assessment').show(1000)
+    });
+
 
 });//end ready
 
